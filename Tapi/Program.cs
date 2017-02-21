5605b20fd935631818313d4f45f5ce4484fff495
Ayaz <ayazzali@mail.ru>
2017-02-21 13:30:41 +0300

    123

--- Tapi/Program.cs
+++ Tapi/Program.cs
@@ -3,93 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TeleSharp.TL;
-using TeleSharp.TL.Messages;
 using TLSharp.Core;
+using TeleSharp.TL;
 
-namespace Tapi
+namespace ConsoleApplication1
 {
     class Program
     {
-        static int apiId = 77245;
-        static string apiHash = "8bb5f0761bab74261e285f0b691e191b";
-        static string user_number = "79393302980";
-
+        static int apiId = 00000;//https://my.telegram.org/auth?to=apps
+        static string apiHash = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";//https://my.telegram.org/auth?to=apps
+        static string user_number = "xxxxxxxxxxx";//Your Phone Number
+        static string phoneNumber = user_number;
         static void Main(string[] args)
         {
-            Console.Write("0");
-            a();
-            Console.Write("-1");
-            
-            Console.ReadKey(true);
+            a() ;
+            Console.ReadKey();
         }
-
-        static public async void  a()
+        static async void a()
         {
-            // System.IO.File.Delete("session.dat");
-            var session = new FileSessionStore();
-            //var p = session.Load("SkySession");
-            //p.SessionExpires = 2147483647; ConfigurationManager.AppSettings[
-            var client = new TelegramClient( apiId, apiHash,session, "SkySession");
+            var store = new FileSessionStore();
+            var client = new TelegramClient(apiId, apiHash, store, "session");// or C:\\Temp\\mySession
             await client.ConnectAsync();
             if (!client.IsUserAuthorized())
             {
-                var hash = await client.SendCodeRequestAsync(user_number);
-                Console.Write("Введите код");
-                var code = Console.ReadLine(); // you can change code in debugger
-                var user = await client.MakeAuthAsync(user_number, hash, code);
+                var hash = await client.SendCodeRequestAsync(phoneNumber); //отсылаем запрос на создании сессии
+
+                var code = Console.ReadLine(); // код который придет от Telegram 
+
+                var user = await client.MakeAuthAsync(phoneNumber, hash, code); // создаем сессию
+                
             }
-            //get available contacts
-            var result = await client.GetContactsAsync();
-            //var dialogs = await client.GetUserDialogsAsync() As TLDialogResult;
-            /*var chat = dialogs.chats.lists
-.Where(c => c.GetType() == typeof(TLChannel))
-.Cast()
-.FirstOrDefault(c => c.username == "<channel_id>");*/
-            //Thank you, but not "TLDialogResult". Use "TLDialogsSlice".
+
+            //var userByPhoneId = await client.("791812312323"); // импорт по номеру телефона
+            //var userByUserNameId = await await client.ImportByUserName("userName"); // импорт по юзернейму
+
+            var s = await client.IsPhoneRegisteredAsync("xxxxxxxxxxx");//WHO WRITE//WHO WRITE
+
+            var f = await client.GetContactsAsync();
+            Console.WriteLine("контакты");
+            f.contacts.lists.ForEach(_ => { Console.WriteLine(_.user_id); });
+            
             TeleSharp.TL.Contacts.TLRequestImportContacts requestImportContacts = new TeleSharp.TL.Contacts.TLRequestImportContacts();
             requestImportContacts.contacts = new TLVector<TLInputPhoneContact>();
-            int num3 = 0;
-            //foreach (string str in source)
+            requestImportContacts.contacts.lists.Add(new TLInputPhoneContact()
             {
-                requestImportContacts.contacts.lists.Add(new TLInputPhoneContact()
-                {
-                    phone = "79393986878",//WHO WRITE//WHO WRITE//WHO WRITE//WHO WRITE//WHO WRITE?
-                    first_name = num3.ToString(),
-                    last_name = num3.ToString()
-                });
-                ++num3;
-            }
-            //isregistered 
-            var o = client.SendRequestAsync<TeleSharp.TL.Contacts.TLImportedContacts>((TLMethod)requestImportContacts);
-            o.Wait();
-            var oo=o.Result.users.lists;
-            //foreach (object list in (await ) ;
-
-            //TLAbsUpdates tlAbsUpdates = await Program.client.SendMessageAsync((TLAbsInputPeer)tlInputPeerUser1, this.textBox2.Text);
-
-
-
-            //var result2 = await client.SearchUserAsync("79518954606");
-            //var y = await client.GetUserDialogsAsync() as TLDialogsSlice;
-            
-
-            //var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
-            //var chat = dialogs.chats.lists
-            //    .OfType<TLChannel>()
-            //    .FirstOrDefault(c => c.title == "TestGroup");
-
-            //find recipient in contacts
-
-            //var userByPhoneId = await client.ImportContactByPhoneNumber("791812312323"); //import by phone
-
-            //var userByUserNameId = await await client.ImportByUserName("userName"); //import by username
-
-            //send message
-            var d=await client.SendMessageAsync(new TLInputPeerUser() { user_id = (oo.First() as TLUser).id }, "123123123");
-            var ааа = d;
-            Console.ReadKey();
-            client.Dispose();
+                phone = "xxxxxxxxxxx",//WHO WRITE//WHO WRITE//WHO WRITE//WHO WRITE//WHO WRITE?
+                first_name = "",
+                last_name = ""
+            });
+            var o = await client.SendRequestAsync<TeleSharp.TL.Contacts.TLImportedContacts>((TLMethod)requestImportContacts);
+            var NewUserId = (o.users.lists.First() as TLUser).id;
+            var d = await client.SendMessageAsync(new TLInputPeerUser() { user_id = NewUserId }, "text xxx");
+            //return true;
         }
     }
 }
